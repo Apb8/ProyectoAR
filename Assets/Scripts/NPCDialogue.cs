04dea16a50c6fbb26764dd6edd79de1184eba3e9
Lexi <119694326+Apb8@users.noreply.github.com>
2025-03-27 13:02:28 +0100

    more updates and wprk on dialogue system and player inputs

--- Assets/Scripts/NPCDialogue.cs
+++ Assets/Scripts/NPCDialogue.cs
@@ -1,35 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
+using UnityEngine.InputSystem;
+
 
 public class NPCDialogue : MonoBehaviour
 {
     public Dialogue dialogueData;
-    private bool playerInRange; //revisar com ho farem
+    //private bool playerInRange; //revisar com ho farem
+    private Transform playerCamera;
+    public float interactionDistance = 1.5f;
+    private PlayerInputActions inputActions;
 
-    private void Update()
+    private void Start()
     {
-        if (playerInRange && Input.GetKeyDown(KeyCode.E))//ja ho canviarem deixo aixo per debug
+        playerCamera = Camera.main.transform; //Asigna la AR camera automaticamente - revisar
+        inputActions = new PlayerInputActions();
+        inputActions.Player.Interact.performed += ctx => TryStartDialogue();
+    }
+
+    private void OnEnable() => inputActions.Enable();
+    private void OnDisable() => inputActions.Disable();
+
+    private void TryStartDialogue()
+    {
+        if (Vector3.Distance(transform.position, playerCamera.position) < interactionDistance)
         {
             DialogueManager.Instance.StartDialogue(dialogueData);
         }
     }
+    //private void Update()
+    //{
+    //    //if (playerInRange && Input.GetKeyDown(KeyCode.E))//ja ho canviarem deixo aixo per debug
+    //    //{
+    //    //    DialogueManager.Instance.StartDialogue(dialogueData);
+    //    //}
+    //    if (Vector3.Distance(transform.position, playerCamera.position) < interactionDistance)
+    //    {
+    //        if (Input.GetKeyDown(KeyCode.E))//para debug en pc, luego se cambia
+    //        {
+    //            DialogueManager.Instance.StartDialogue(dialogueData);
+    //        }
+    //    }
+    //}
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            playerInRange = true;
-        }
-    }
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.CompareTag("Player"))
+    //    {
+    //        playerInRange = true;
+    //    }
+    //}
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            playerInRange = false;
-        }
-    }
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (other.CompareTag("Player"))
+    //    {
+    //        playerInRange = false;
+    //    }
+    //}
 }
---
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    private PlayerInputActions inputActions;
+
+    private void Awake()
+    {
+        inputActions = new PlayerInputActions();
+        inputActions.Player.Interact.performed += ctx => Interact();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+    private void Interact()
+    {
+        Debug.Log("¡Interactuar!");
+        // Aquí puedes llamar al DialogueManager para iniciar un diálogo
+    }
+}
+
