c05b1843e42998a343a729652df002c36e40a104
Lexi <119694326+Apb8@users.noreply.github.com>
2025-04-14 23:45:16 +0200

    removed billboard since it keeps breaking the dialogue anchor

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -4,7 +4,10 @@ public class Billboard : MonoBehaviour
 {
     void LateUpdate()
     {
-        if (Camera.main != null)
-            transform.LookAt(Camera.main.transform);
+        if (Camera.main == null) return;
+
+        Vector3 lookDirection = transform.position - Camera.main.transform.position;
+        lookDirection.y = 0; // evitar que gire hacia arriba/abajo pero se puede quitar
+        transform.rotation = Quaternion.LookRotation(lookDirection);
     }
 }
